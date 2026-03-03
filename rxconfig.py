3e12ef940e375d23cfad9ef273797a514042d62e
Mauricio Payetta <mauricio@xoor.io>
2026-03-03 09:49:27 -0300

    Fix: disable SSR prerendering to fix vaul document is not defined

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,7 @@ import reflex as rx
 config = rx.Config(
     app_name="cont_synth",
     api_url=os.environ.get("API_URL", "http://localhost:8000"),
+    prerender=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
