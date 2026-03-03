a9c70a540a58e38a7d7915ca4c40287dfaaa0c69
Mauricio Payetta <mauricio@xoor.io>
2026-03-03 13:20:31 -0300

    Remove "Built with Reflex" button in prod mode

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,7 @@ import reflex as rx
 config = rx.Config(
     app_name="cont_synth",
     api_url=os.environ.get("API_URL", "http://localhost:8000"),
+    show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
